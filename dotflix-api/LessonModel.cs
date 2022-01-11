ec28542b7d47f0013aff210762cfc21c47197c70
Jean Michel <jeanbertagna@gmail.com>
2022-01-11 16:34:35 -0400

    Add Lesson Model Class

---
+++ dotflix-api/LessonModel.cs
@@ -0,0 +1,19 @@
+﻿using DotFlix.Classes;
+
+namespace dotflix_api
+{
+    public class LessonModel
+    {
+        public int Id { get; protected set; }
+
+        public Category Category { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+        public bool Excluded { get; set; }
+    }
+}
