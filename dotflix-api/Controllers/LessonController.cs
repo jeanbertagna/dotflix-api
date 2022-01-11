b7063082486f4e432682e17d9d41cf59803d92db
Jean Michel <jeanbertagna@gmail.com>
2022-01-11 17:26:45 -0400

    Write all methods http

---
+++ dotflix-api/Controllers/LessonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotflix_api.Controllers
+{
+    [Route("controller")]
+    public class LessonController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Relation()
+        {
+            IList<LessonModel> lesson = new List<LessonModel>();
+
+            return Ok(lesson);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] LessonModel model)
+        {
+            return Ok(model);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) => Ok(id);
+
+        [HttpPost("{id}")]
+        public IActionResult Insert(int id, [FromBody]LessonModel model) => Ok(model);
+
+        [HttpGet("{id}")]
+        public IActionResult Consult(int id) => Ok(id);
+
+    }
+}
